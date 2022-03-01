aea825ebd5e57d75f1c6adc613c299a526009995
taku1219 <acmiran189926@gmail.com>
2022-03-02 07:59:06 +0900

    [add]taskcardtitleの状態管理途中

--- src/components/task/TaskCardTitle.tsx
+++ src/components/task/TaskCardTitle.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const TaskCardTitle = () => {
-  return <div>TaskCardTitle</div>;
+  const [isClick, setIsClick] = useState(false);
+  const [inputCardTitle, setInputCardTitle] = useState('Today');
+
+  const handleClick = () => {
+    setIsClick(true);
+    console.log(isClick);
+  };
+
+  const handleChange = (e: any) => {
+    setInputCardTitle(e.target.value);
+  };
+  return (
+    <div onClick={handleClick}>
+      {isClick ? (
+        <form>
+          <input type='text' onChange={handleChange} />
+        </form>
+      ) : (
+        <h3>Today</h3>
+      )}
+    </div>
+  );
 };
