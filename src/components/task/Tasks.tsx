ca4de3d023293040d0e40308d1c8f8fd04524b35
taku1219 <acmiran189926@gmail.com>
2022-03-05 01:38:00 +0900

    [install] react-beautiful-dndをinstall DragDropContextとDroppableを追加

--- src/components/task/Tasks.tsx
+++ src/components/task/Tasks.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import { Task } from './Task';
 
 type Props = {
@@ -8,12 +9,25 @@ type Props = {
 
 export const Tasks: React.FC<Props> = ({ taskList, setTaskList }) => {
   return (
-    <div>
-      {taskList.map((task) => (
-        <div key={task.id}>
-          <Task task={task} taskList={taskList} setTaskList={setTaskList} />
-        </div>
-      ))}
-    </div>
+    <>
+      <DragDropContext onDragEnd={() => {}}>
+        <Droppable droppableId='droppable'>
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {taskList.map((task) => (
+                <div key={task.id}>
+                  <Task
+                    task={task}
+                    taskList={taskList}
+                    setTaskList={setTaskList}
+                  />
+                </div>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </>
   );
 };
